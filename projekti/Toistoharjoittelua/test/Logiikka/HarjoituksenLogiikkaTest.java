46479990df195b12d098fd809260db075f42faa7
Johanna <johanna@viggo.(none)>
2012-06-11 21:48:19 +0300

    harjoituksenLogiikassa kolme testiä

--- projekti/Toistoharjoittelua/test/Logiikka/HarjoituksenLogiikkaTest.java
+++ projekti/Toistoharjoittelua/test/Logiikka/HarjoituksenLogiikkaTest.java
@@ -12,16 +12,16 @@ import org.junit.*;
  * @author johanna
  */
 public class HarjoituksenLogiikkaTest {
-    
+
     public HarjoituksenLogiikkaTest() {
     }
     HarjoituksenLogiikka harjoituksenLogiikka;
+
     @Before
     public void setUp() {
         harjoituksenLogiikka = new HarjoituksenLogiikka("testikoe");
     }
-    
- 
+
     /**
      * Test of etene method, of class HarjoituksenLogiikka.
      */
@@ -32,7 +32,32 @@ public class HarjoituksenLogiikkaTest {
         HarjoituksenLogiikka instance = harjoituksenLogiikka;
         instance.etene(olikoOikein);
         int expResult = 3;
-        int result = harjoituksenLogiikka.kysyttavat.size();
+        int result = instance.kysyttavat.size();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testKasvaakoNykyinenJosVastausOnVaarin() {
+        System.out.println("etene");
+        boolean olikoOikein = false;
+        HarjoituksenLogiikka instance = harjoituksenLogiikka;
+        instance.etene(olikoOikein);
+        int expResult = 1;
+        int result = instance.nykyinen;
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testNollautuukoNykyinenJosListaMeneeYmpari() {
+        System.out.println("etene");
+        boolean olikoOikein = false;
+        HarjoituksenLogiikka instance = harjoituksenLogiikka;
+        instance.etene(olikoOikein);
+        instance.etene(olikoOikein);
+        instance.etene(olikoOikein);
+        instance.etene(olikoOikein);
+        int expResult = 0;
+        int result = instance.nykyinen;
         assertEquals(expResult, result);
     }
 }
